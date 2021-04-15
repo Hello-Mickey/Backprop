18ed35b44667016a48688dce04d35131c86b0515
Mike Mishchanin <m.mishchanin@smartmealservice.com>
2021-04-15 15:32:15 +0300

    add backprop and fit-predict

--- NN.py
+++ NN.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
+from keras.datasets import mnist
 
 class SimpleNN:
     def __init__(self, layers_list, type='classification'):
@@ -15,12 +17,17 @@ class SimpleNN:
 
         self.__L = len(layers_list)
         self.n = 0
-        self.costs = []
         self.__random_seed = 1
+
+        self.history = {}
+        self.train_params = {}
+        self.data = {}
 
     def sigmoid(self, t): return 1 / (1 + np.exp(-t))
 
-    def sigmoid_derivative(self, p): return p * (p - 1)
+    def sigmoid_derivative(self, Z):
+        p = 1/ (1 + np.exp(-Z))
+        return p * (1 - p)
 
     def softmax(self, Z):
         expZ = np.exp(Z - np.max(Z))
@@ -40,8 +47,8 @@ class SimpleNN:
                 params['b' + str(i)] = np.random.randn(self.layers_size[i], 1)
         else:
             for i in range(1, self.__L):
-                params['W' + str(i)] = np.zeros(self.layers_size[i], self.layers_size[i - 1])
-                params['b' + str(i)] = np.zeros(self.layers_size[i], 1)
+                params['W' + str(i)] = np.zeros((self.layers_size[i], self.layers_size[i - 1]))
+                params['b' + str(i)] = np.zeros((self.layers_size[i], 1))
         return params
 
     def forward(self, X):
@@ -56,10 +63,98 @@ class SimpleNN:
         # last forward step - softmax activation
         self.layers["output"] = self.softmax(np.dot(self.layers["W" + str(self.__L-1)], self.weights["W" + str(self.__L)]) \
                                                         + self.weights["b" + str(self.__L)])
+        return self.layers["output"]
 
     def backward(self, Y):
         d_weights = {}
-        dZ = self.layers["output"] - Y
 
-    def fit(self, x, y):
-        return 0
+        # compute
+        dZ = self.layers["output"] - Y
+        dW = np.dot(dZ, self.layers["W" + str(self.__L - 1)]) / self.n
+        db = np.sum(dZ, axis=1, keepdims=True) / self.n
+
+        dWPrev = self.weights["W" + str(self.__L)]
+
+        d_weights["W" + str(self.__L)] = dW
+        d_weights["b" + str(self.__L)] = db
+
+        for l in range(self.__L-1, 0, -1):
+            dZ = np.dot(self.weights["W" + str(l+1)], dZ) \
+                    * self.sigmoid_derivative(self.layers["W" + str(l)])
+            dW = 1. / self.n * np.dot(dZ, np.dot(self.layers["W" + str(l-1)], self.weights["W" + str(l)]) \
+                                                        + self.weights["b" + str(l)])
+            db = 1. / self.n * np.sum(dZ, axis=1, keepdims=True)
+
+            d_weights["W" + str(l)] = dW
+            d_weights["b" + str(l)] = db
+
+        # update
+        for l in range(self.__L):
+            self.weights["W" + str(l)] = self.weights["W" + str(l)] - self.train_params["lr"] * d_weights["W" + str(l)]
+            self.weights["b" + str(l)] = self.weights["b" + str(l)] - self.train_params["lr"] * d_weights["b" + str(l)]
+
+    def fit(self, x, y, lr = 0.01, n_iters = 2000):
+        self.train_params["lr"] = lr
+        self.train_params["epochs"] = n_iters
+        self.data["X"] = x
+        self.data["Y"] = y
+        self.n = self.data["X"].shape[0]
+        self.init_params()
+        self.history["loss"] = []
+        self.history["acc"] = []
+
+        for epoch in self.train_params["epochs"]:
+            self.forward(self.data["X"])
+            cost = self.cross_entropy_loss(self.data["Y"], self.layers["output"])
+            if epoch%10 == 0:
+                self.history["loss"].append(cost)
+            if epoch%100 == 0:
+                pred = self.predict(self.data["X"], self.data["Y"])
+                self.history["acc"].append(pred)
+                print("CrossEntropyLoss: ", cost, "train Accuracy: ", pred)
+
+    def predict(self, X, Y):
+        output = self.forward(X)
+        y_out = np.argmax(output, axis=0)
+        y = np.argmax(Y, axis=1)
+        acc = (y_out == y).mean()
+        return acc
+
+    def plot_loss(self):
+        plt.plot(np.arange(len(self.history["loss"])), self.history["loss"])
+        plt.xlabel("epochs")
+        plt.ylabel("cost")
+        plt.show()
+
+    def clean_memory(self):
+        del self.data
+        del self.layers
+
+def pre_process_data(train_x, train_y, test_x, test_y):
+    # Normalize
+    train_x = train_x / 255.
+    test_x = test_x / 255.
+
+    enc = OneHotEncoder(sparse=False, categories='auto')
+    train_y = enc.fit_transform(train_y.reshape(len(train_y), -1))
+
+    test_y = enc.transform(test_y.reshape(len(test_y), -1))
+
+    return train_x, train_y, test_x, test_y
+
+if __name__ == '__main__':
+    data = mnist.load_data()
+    train_x, train_y, test_x, test_y = data[0][0], data[0][1], data[1][0], data[1][1]
+
+    train_x, train_y, test_x, test_y = pre_process_data(train_x, train_y, test_x, test_y)
+
+    print("train_x's shape: " + str(train_x.shape))
+    print("test_x's shape: " + str(test_x.shape))
+
+    layers_dims = [50, 10]
+
+    snn = SimpleNN(layers_dims)
+    snn.fit(train_x, train_y, lr=0.1, n_iters=1000)
+    print("Train Accuracy:", snn.predict(train_x, train_y))
+    print("Test Accuracy:", snn.predict(test_x, test_y))
+    snn.plot_loss()
